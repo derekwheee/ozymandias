4b6cd0556a20434cf9f3c4c071d19ba7a1b139be
Derek Wheelden <derek.wheelden@gmail.com>
2015-09-14 21:33:04 -0400

    Board cleanup, add test module

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var Dispatch = require('./lib/dispatch');
 var Board = require('./lib/board');
+var TestBoard = require('./lib/modules/test');
 
-var board = new Board();
+require('dotenv').load();
+
+var dispatch = new Dispatch();
+var board = new Board(dispatch);
+var test = new TestBoard(dispatch);
